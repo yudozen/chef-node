95f8e81df3babf1d51549d91b8e397dfe8966d3e
vagrant <vagrant@Hadoop-berkshelf.(none)>
2013-02-26 17:10:38 +0100

    initial

---
+++ recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: node
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+git "/tmp/node" do
+	repository "git://github.com/joyent/node.git"
+	reference "master"
+	action :sync
+end
+
+bash "install_node" do
+	user "root"
+	cwd "/tmp/node"
+	code <<-EOH
+		(./configure && make && make install)	
+	EOH
+end
